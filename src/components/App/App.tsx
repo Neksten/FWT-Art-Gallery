34d4d74a1c8d80157e02a47600f2f7f21397a050
Neksten <denchik7366@gmail.com>
2023-08-21 18:55:27 +0400

    added eslint config, added prettier config, added variables for fonts, colors, breakpoints

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const App: React.FC = () => (
+  <div className={styles.app}>
+    <div className={styles.container}>App</div>
+  </div>
+);
+
+export default App;

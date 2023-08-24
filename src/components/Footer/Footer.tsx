c314868218074c440bd4b07f78a30cfe9626ac69
Neksten <denchik7366@gmail.com>
2023-08-24 18:35:22 +0400

    Added the footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,43 @@
+import { FC } from "react";
+import classNames from "classnames/bind";
+
+import { useTheme } from "../../hooks/useTheme";
+import MyLink from "../../ui/MyLink/MyLink";
+import { Fb } from "../../assets/Fb";
+import { Inst } from "../../assets/Inst";
+import { Vk } from "../../assets/Vk";
+
+import styles from "./styles.module.scss";
+
+const cx = classNames.bind(styles);
+
+const Footer: FC = () => {
+  const { theme } = useTheme();
+  return (
+    <footer
+      className={cx(`${styles.footer}`, {
+        dark: theme === "dark",
+      })}
+    >
+      <div className={`${styles.footer__content} container`}>
+        <div className={styles.footer__left}>
+          <div className={styles.footer__info}>
+            <p className={styles.footer__text}>
+              Проект реализован в рамках стажировки для Frontend-разработчиков
+              от компании
+            </p>
+            <MyLink to="https://framework.team/">Framework Team</MyLink>
+          </div>
+          <p className={styles.footer__name}>Иванов Иван, 2021</p>
+        </div>
+        <div className={styles.footer__socials}>
+          <Fb />
+          <Inst />
+          <Vk />
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

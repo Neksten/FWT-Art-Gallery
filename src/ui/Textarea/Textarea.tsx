2920f5f61d4fd7f66561d01c7648a908cc31089b
Neksten <denchik7366@gmail.com>
2023-09-15 17:18:45 +0400

    Added the Textarea component and changed the Input component

--- src/ui/Textarea/Textarea.tsx
+++ src/ui/Textarea/Textarea.tsx
@@ -1,34 +1,42 @@
-import { forwardRef, InputHTMLAttributes } from "react";
+import { forwardRef, TextareaHTMLAttributes } from "react";
+import classNames from "classnames/bind";
 
 import { ReactComponent as Error } from "@/assets/icons/error.svg";
-import classNames from "classnames/bind";
 
 import styles from "./styles.module.scss";
 
 const cx = classNames.bind(styles);
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   label: string;
   htmlFor: string;
   error?: string;
   theme?: "light" | "dark";
 }
 
-const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ label, htmlFor, theme = "light", error, value, ...props }, ref) => {
+const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  (
+    { className, label, htmlFor, value, error, theme = "light", ...props },
+    ref
+  ) => {
     return (
       <div className={cx("text-field", `text-field-${theme}`)}>
         <label className={cx("text-field__label")} htmlFor={htmlFor}>
           {label}
         </label>
-        <input
+        <textarea
           {...props}
+          name={htmlFor}
           ref={ref}
           value={value}
           className={cx(
-            "text-field__input",
-            `text-field__input-${theme}`,
-            error ? `text-field__input-error` : ""
+            "text-field__textarea",
+            `text-field__textarea-${theme}`,
+            "scroll",
+            className,
+            {
+              "text-field__textarea-error": error,
+            }
           )}
         />
         {error && (
@@ -42,4 +50,4 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
   }
 );
 
-export default Input;
+export default Textarea;

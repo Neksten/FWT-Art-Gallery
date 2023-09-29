e519e12f285ae8f996f92e094b05e51621f7c831
Neksten <denchik7366@gmail.com>
2023-09-29 16:05:57 +0400

    Edited pages

--- src/pages/Artist/Artist.tsx
+++ src/pages/Artist/Artist.tsx
@@ -4,14 +4,27 @@ import classNames from "classnames/bind";
 
 import { useAppSelector } from "@/hooks/redux";
 import { useGetArtistByIdQuery } from "@/store/artists/artist.api";
+import { useGetGenresQuery } from "@/store/genre/genre.api";
 import { useTheme } from "@/context/ThemeContext";
-import { Button } from "@/ui/Button";
+import { IArtistModal } from "@/models/IArtist";
+
+import { ArtistModal } from "@/components/ArtistModal";
+import { DeleteModal } from "@/components/DeleteModal";
+import { PaintingModal } from "@/components/PaintingModal";
+import { CardPainting } from "@/components/CardPainting";
 import { Card } from "@/ui/Card";
+import { IconButton } from "@/ui/IconButton";
+import { Button } from "@/ui/Button";
 import { GridLayout } from "@/ui/GridLayout";
 import { Accordion } from "@/ui/Accordion";
 import { Genre } from "@/ui/Genre";
 import { Loader } from "@/ui/Loader";
+import { NoImage } from "@/ui/NoImage";
 import { ReactComponent as ArrowBack } from "@/assets/icons/arrow-back.svg";
+import { ReactComponent as Delete } from "@/assets/icons/delete.svg";
+import { ReactComponent as Edit } from "@/assets/icons/edit.svg";
+import { ReactComponent as Plus } from "@/assets/icons/plus.svg";
+import { ReactComponent as Photo } from "@/assets/icons/photo.svg";
 
 import styles from "./styles.module.scss";
 
@@ -21,17 +34,51 @@ const Artist: FC = () => {
   const { id } = useParams();
   const { theme } = useTheme();
   const { isAuth } = useAppSelector((state) => state.authSlice);
-  const [isOpenDescription, setIsOpenDescriptions] = useState<boolean>(false);
+  const [isOpenDelete, setIsOpenDelete] = useState(false);
+  const [isOpenModalArtist, setIsOpenModalArtist] = useState(false);
+  const [isOpenModalPainting, setIsOpenModalPainting] = useState(false);
+  const [isOpenDescription, setIsOpenDescriptions] = useState(false);
   const { data } = useGetArtistByIdQuery(
     { isAuth, id: id || "" },
     { skip: isAuth === null }
   );
+  const { data: genresData } = useGetGenresQuery(
+    { isAuth },
+    { skip: isAuth === null }
+  );
+
+  const initialData: IArtistModal = {
+    name: data?.name || "",
+    yearsOfLife: data?.yearsOfLife || "",
+    description: data?.description || "",
+    genres: data?.genres || [],
+    avatar: data?.avatar
+      ? `${process.env.REACT_APP_BASE_URL}${data.avatar.src}`
+      : "",
+  };
 
   return (
     <main className={cx("artist", `artist-${theme}`)}>
+      {isAuth && isOpenDelete && (
+        <DeleteModal
+          title="Do you want to delete this artist profile?"
+          setIsOpen={setIsOpenDelete}
+        />
+      )}
+      {isAuth && genresData && isOpenModalArtist && (
+        <ArtistModal
+          initialData={initialData}
+          idArtist={id}
+          genresList={genresData}
+          setIsOpen={setIsOpenModalArtist}
+        />
+      )}
+      {isAuth && isOpenModalPainting && (
+        <PaintingModal idArtist={id || ""} setIsOpen={setIsOpenModalPainting} />
+      )}
       {data ? (
         <div className={cx("artist__content", "container")}>
-          <nav className={cx("artist__menu")}>
+          <nav className={cx("artist__menu", "artist-menu")}>
             <Link to="/">
               <div className={cx("artist__back")}>
                 <div className={cx("artist__arrow")}>
@@ -47,6 +94,19 @@ const Artist: FC = () => {
                 </Button>
               </div>
             </Link>
+            {isAuth && (
+              <div className={cx("artist-menu__right")}>
+                <IconButton
+                  onClick={() => setIsOpenModalArtist(true)}
+                  theme={theme}
+                >
+                  <Edit />
+                </IconButton>
+                <IconButton onClick={() => setIsOpenDelete(true)} theme={theme}>
+                  <Delete />
+                </IconButton>
+              </div>
+            )}
           </nav>
           <section className={cx("artist__hero", "artist-hero")}>
             <div className={cx("artist-hero__info", "artist-hero-info")}>
@@ -71,32 +131,83 @@ const Artist: FC = () => {
                 </div>
               </div>
             </div>
-            <div className={cx("artist-hero__image-container")}>
-              {data.avatar && (
+            <div
+              className={cx("artist-hero__image-container", {
+                empty: !data?.avatar?.src,
+              })}
+            >
+              {data.avatar ? (
                 <img
                   src={`${process.env.REACT_APP_BASE_URL}${data.avatar.src}`}
                   alt="artist"
                   className={cx("artist-hero__image")}
                 />
+              ) : (
+                <NoImage variant="big" theme={theme} />
               )}
             </div>
           </section>
           <section className={cx("artist__artworks", "artist-artworks")}>
             <h3 className={cx("artist-artworks__title")}>Artworks</h3>
-            <div className={cx("artist-artworks__paintings")}>
-              <GridLayout>
-                {data.paintings.map((painting) => (
-                  <Card
-                    key={painting._id}
-                    to="#"
-                    title={painting.name}
-                    years={painting.yearOfCreation}
-                    imgUrl={`${process.env.REACT_APP_BASE_URL}${painting.image.src}`}
-                    theme={theme}
-                  />
-                ))}
-              </GridLayout>
-            </div>
+            <nav className={cx("artist-artworks__menu")}>
+              {isAuth && (
+                <Button
+                  onClick={() => setIsOpenModalPainting(true)}
+                  variant="outlined"
+                  theme={theme}
+                  startIcon={<Plus />}
+                  className={cx("artist-artworks__button")}
+                >
+                  Add picture
+                </Button>
+              )}
+            </nav>
+            {data.paintings.length > 0 ? (
+              <div className={cx("artist-artworks__paintings")}>
+                <GridLayout>
+                  {data.paintings.map((painting) =>
+                    isAuth ? (
+                      <CardPainting
+                        key={painting._id}
+                        title={painting.name}
+                        years={painting.yearOfCreation}
+                        imgUrl={`${process.env.REACT_APP_BASE_URL}${painting.image.src}`}
+                        paintingId={painting._id}
+                        mainPaintingId={data.mainPainting._id}
+                      />
+                    ) : (
+                      <Card
+                        key={painting._id}
+                        title={painting.name}
+                        years={painting.yearOfCreation}
+                        imgUrl={`${process.env.REACT_APP_BASE_URL}${painting.image.src}`}
+                        theme={theme}
+                      />
+                    )
+                  )}
+                </GridLayout>
+              </div>
+            ) : (
+              <>
+                <button
+                  type="button"
+                  className={cx(
+                    "artist-artworks__empty",
+                    "artist-artworks-empty"
+                  )}
+                >
+                  <div className={cx("artist-artworks-empty__icon")}>
+                    <Photo />
+                  </div>
+                  <div className={cx("artist-artworks-empty__add")}>
+                    <Plus />
+                  </div>
+                </button>
+                <h4 className={cx("artist-artworks__explanation", "md")}>
+                  The paintings of this artist have not been uploaded yet.
+                </h4>
+              </>
+            )}
           </section>
         </div>
       ) : (
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,17 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import classNames from "classnames/bind";
 
 import { useTheme } from "@/context/ThemeContext";
 import { useAppSelector } from "@/hooks/redux";
 import { useGetArtistsQuery } from "@/store/artists/artist.api";
+import { useGetGenresQuery } from "@/store/genre/genre.api";
+
+import { ArtistModal } from "@/components/ArtistModal";
+import { CardLink } from "@/components/CardLink";
 import GridLayout from "@/ui/GridLayout/GridLayout";
-import Card from "@/ui/Card/Card";
 import { Loader } from "@/ui/Loader";
+import { Button } from "@/ui/Button";
+import { ReactComponent as Plus } from "@/assets/icons/plus.svg";
 
 import styles from "./styles.module.scss";
 
@@ -15,26 +20,52 @@ const cx = classNames.bind(styles);
 const Home: FC = () => {
   const { theme } = useTheme();
   const { isAuth } = useAppSelector((state) => state.authSlice);
-  const { data, isLoading } = useGetArtistsQuery(
+  const [isOpenAdd, setIsOpenAdd] = useState(false);
+  const { data: artistsData, isLoading } = useGetArtistsQuery(
+    { isAuth },
+    { skip: isAuth === null }
+  );
+  const { data: genresData } = useGetGenresQuery(
     { isAuth },
     { skip: isAuth === null }
   );
 
-  const artists = data?.data;
-
+  const artists = artistsData?.data;
   return (
     <main className={cx("home", `home-${theme}`)}>
+      {isAuth && isOpenAdd && genresData && (
+        <ArtistModal
+          genresList={genresData}
+          variant="add"
+          setIsOpen={setIsOpenAdd}
+        />
+      )}
       {!isLoading && artists ? (
         <section className={cx("home__content", "container")}>
+          <nav className={cx("home__menu")}>
+            {isAuth && (
+              <Button
+                onClick={() => setIsOpenAdd(true)}
+                variant="outlined"
+                startIcon={<Plus />}
+                theme={theme}
+              >
+                Add Artist
+              </Button>
+            )}
+          </nav>
           <GridLayout>
             {artists.map((item) => (
-              <Card
+              <CardLink
                 to={`/artist/${item._id}`}
                 key={item._id}
                 title={item.name}
                 years={item.yearsOfLife}
-                imgUrl={`${process.env.REACT_APP_BASE_URL}${item.mainPainting.image.src}`}
-                theme={theme}
+                imgUrl={
+                  item.mainPainting
+                    ? `${process.env.REACT_APP_BASE_URL}${item.mainPainting.image.src}`
+                    : ""
+                }
               />
             ))}
           </GridLayout>

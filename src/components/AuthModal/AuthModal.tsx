86028504180d5c7a6db26f5b5f681a775f40d939
Neksten <denchik7366@gmail.com>
2023-09-29 16:02:22 +0400

    Changed the modal authorization window

--- src/components/AuthModal/AuthModal.tsx
+++ src/components/AuthModal/AuthModal.tsx
@@ -1,23 +1,21 @@
-import { Dispatch, FC, SetStateAction, useState } from "react";
+import { FC } from "react";
 import classNames from "classnames/bind";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 import { useTheme } from "@/context/ThemeContext";
-import { Modal } from "@/components/Modal";
-import { Input } from "@/ui/Input";
-import { MyLink } from "@/ui/MyLink";
-import { Button } from "@/ui/Button";
-import loginBg from "@/assets/login.jpg";
-import registrationBg from "@/assets/registration.jpg";
-import { ReactComponent as Close } from "@/assets/icons/close.svg";
 import { IAuthInputs } from "@/models/Auth";
 import { useLoginMutation, useRegisterMutation } from "@/store/auth/auth.api";
-
 import { useAppDispatch } from "@/hooks/redux";
 import { setIsAuth } from "@/store/auth/authSlice";
 import { getFingerprint } from "@/utils/auth/getFingerprint";
+
+import { Modal } from "@/components/Modal";
+import { Input } from "@/ui/Input";
+import { Button } from "@/ui/Button";
+import loginBg from "@/assets/login.jpg";
+import registrationBg from "@/assets/registration.jpg";
 
 import styles from "./styles.module.scss";
 
@@ -27,8 +25,6 @@ interface AuthorizationProps {
   setIsOpen: (value: boolean) => void;
   setIsRedirect: () => void;
   variant?: "login" | "signup";
-  menuClass: "open" | "delete";
-  setMenuClass: (value: "open" | "delete") => void;
 }
 
 const validationSchema = yup.object({
@@ -42,72 +38,39 @@ const validationSchema = yup.object({
     .required("Required field"),
 });
 
-const initialFormValue: IAuthInputs = {
-  username: "",
-  password: "",
-};
-
 const AuthModal: FC<AuthorizationProps> = ({
   setIsOpen,
   setIsRedirect,
-  setMenuClass,
   variant = "login",
-  menuClass,
 }) => {
   const { theme } = useTheme();
   const dispatch = useAppDispatch();
-  const [loginForm, setLoginForm] = useState(initialFormValue);
-  const [signupForm, setSignupForm] = useState(initialFormValue);
   const [registerRequest] = useRegisterMutation();
   const [loginRequest] = useLoginMutation();
   const {
     register,
     handleSubmit,
-    reset,
     formState: { errors },
   } = useForm<IAuthInputs>({
     resolver: yupResolver(validationSchema),
     mode: "onBlur",
   });
 
-  const closeForm = () => {
-    setMenuClass("delete");
-    setTimeout(() => {
-      setMenuClass("open");
-      setIsOpen(false);
-      reset({ username: "", password: "" });
-      if (variant === "login") {
-        setLoginForm(initialFormValue);
-      } else {
-        setSignupForm(initialFormValue);
-      }
-    }, 300);
-  };
   const fulfilledRequest = () => {
     dispatch(setIsAuth(true));
-    closeForm();
-  };
-  const handleChangeState = (
-    setForm: Dispatch<SetStateAction<IAuthInputs>>,
-    name: string,
-    value: string
-  ) => {
-    setForm((prev: IAuthInputs) => ({
-      ...prev,
-      [name]: value,
-    }));
+    setIsOpen(false);
   };
 
-  const onSubmitHandler = async () => {
+  const onSubmitHandler = async (data: any) => {
     const fingerprint = await getFingerprint();
 
     if (variant === "login") {
-      await loginRequest({ ...loginForm, fingerprint })
+      await loginRequest({ ...data, fingerprint })
         .unwrap()
         .then(fulfilledRequest)
         .catch(() => {});
     } else {
-      await registerRequest({ ...signupForm, fingerprint })
+      await registerRequest({ ...data, fingerprint })
         .unwrap()
         .then(fulfilledRequest)
         .catch(() => {});
@@ -115,109 +78,61 @@ const AuthModal: FC<AuthorizationProps> = ({
   };
 
   const handleClickRedirect = () => {
-    closeForm();
+    setIsOpen(false);
     setIsRedirect();
   };
+
   return (
-    <Modal menuClass={menuClass} closeModal={closeForm}>
-      <div className={cx("authorization", `authorization-${theme}`, menuClass)}>
-        <div className={cx("authorization__image-container")}>
-          <img
-            src={variant === "login" ? loginBg : registrationBg}
-            alt="authorization"
-            className={cx("authorization__image")}
-          />
-        </div>
-        <div className={cx("authorization__info")}>
-          <div
-            aria-hidden
-            onClick={closeForm}
-            className={cx(
-              "authorization__close",
-              `authorization__close-${theme}`
-            )}
-          >
-            <Close />
+    <Modal
+      className={cx("modal", `modal-${theme}`)}
+      closeModal={() => setIsOpen(false)}
+    >
+      <div className={cx("modal__image-container")}>
+        <img
+          src={variant === "login" ? loginBg : registrationBg}
+          alt={variant}
+          className={cx("modal__image")}
+        />
+      </div>
+      <div className={cx("modal__info")}>
+        <h3 className={cx("modal__title")}>
+          {variant === "login" ? "Welcome back" : "Create your profile"}
+        </h3>
+        <form
+          onSubmit={handleSubmit(onSubmitHandler)}
+          className={cx("modal__form")}
+        >
+          <div className={cx("modal__inputs")}>
+            <Input
+              {...register("username")}
+              label="email"
+              theme={theme}
+              error={errors?.username?.message}
+            />
+            <Input
+              {...register("password")}
+              label="password"
+              theme={theme}
+              error={errors?.password?.message}
+            />
           </div>
-          <h3 className={cx("authorization__title")}>
-            {variant === "login" ? "Welcome back" : "Create your profile"}
-          </h3>
-          <form
-            onSubmit={handleSubmit(onSubmitHandler)}
-            className={cx("authorization__form")}
+          <Button type="submit" theme={theme}>
+            Log In
+          </Button>
+        </form>
+        <div className={cx("modal__description")}>
+          <p className={cx("modal__text", "small", "lh")}>
+            {variant === "login"
+              ? "If you don't have an account yet, please"
+              : "If you already have an account, please"}
+          </p>
+          <button
+            type="button"
+            className={cx("modal__link")}
+            onClick={handleClickRedirect}
           >
-            <div className={cx("authorization__inputs")}>
-              <Input
-                {...register("username")}
-                type="text"
-                label="email"
-                htmlFor="email"
-                theme={theme}
-                error={errors.username ? errors.username.message : ""}
-                value={
-                  variant === "login" ? loginForm.username : signupForm.username
-                }
-                onChange={
-                  variant === "login"
-                    ? (e) =>
-                        handleChangeState(
-                          setLoginForm,
-                          "username",
-                          e.target.value
-                        )
-                    : (e) =>
-                        handleChangeState(
-                          setSignupForm,
-                          "username",
-                          e.target.value
-                        )
-                }
-              />
-              <Input
-                {...register("password")}
-                type="text"
-                label="password"
-                htmlFor="password"
-                theme={theme}
-                error={errors.password ? errors.password.message : ""}
-                value={
-                  variant === "login" ? loginForm.password : signupForm.password
-                }
-                onChange={
-                  variant === "login"
-                    ? (e) =>
-                        handleChangeState(
-                          setLoginForm,
-                          "password",
-                          e.target.value
-                        )
-                    : (e) =>
-                        handleChangeState(
-                          setSignupForm,
-                          "password",
-                          e.target.value
-                        )
-                }
-              />
-            </div>
-            <Button type="submit" theme={theme}>
-              Log In
-            </Button>
-          </form>
-          <div className={cx("authorization__description")}>
-            <p className={cx("authorization__text", "small", "lh")}>
-              {variant === "login"
-                ? "If you don't have an account yet, please"
-                : "If you already have an account, please"}
-            </p>
-            <MyLink
-              className={cx("authorization__link")}
-              onClick={handleClickRedirect}
-              to=""
-            >
-              {variant === "login" ? "sign up" : "log in"}
-            </MyLink>
-          </div>
+            {variant === "login" ? "sign up" : "log in"}
+          </button>
         </div>
       </div>
     </Modal>

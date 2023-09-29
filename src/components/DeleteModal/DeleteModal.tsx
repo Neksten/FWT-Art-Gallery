0edb8845ee973ec74b5d7b3d092e428e88e4f7aa
Neksten <denchik7366@gmail.com>
2023-09-29 16:02:58 +0400

    Added a modal window for deleting an artist

--- src/components/DeleteModal/DeleteModal.tsx
+++ src/components/DeleteModal/DeleteModal.tsx
@@ -1,70 +1,64 @@
-import { FC, useState } from "react";
-import { Modal } from "@/components/Modal";
+import { FC } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import classNames from "classnames/bind";
 
-import { ReactComponent as Close } from "@/assets/icons/close.svg";
-import { ReactComponent as Delete } from "@/assets/icons/delete.svg";
-
-import { Button } from "@/ui/Button";
 import { useTheme } from "@/context/ThemeContext";
 import { useDeleteArtistByIdMutation } from "@/store/artists/artist.api";
-import { useParams } from "react-router-dom";
+
+import { Modal } from "@/components/Modal";
+import { Button } from "@/ui/Button";
+import { ReactComponent as Delete } from "@/assets/icons/delete.svg";
+
 import styles from "./styles.module.scss";
 
 const cx = classNames.bind(styles);
 
 interface DeleteModalProps {
   setIsOpen: (value: boolean) => void;
+  title: string;
 }
 
-const DeleteModal: FC<DeleteModalProps> = ({ setIsOpen }) => {
+const DeleteModal: FC<DeleteModalProps> = ({ setIsOpen, title }) => {
   const { id } = useParams();
   const { theme } = useTheme();
-  const [menuClass, setMenuClass] = useState<"open" | "delete">("open");
+  const navigate = useNavigate();
   const [deleteArtist] = useDeleteArtistByIdMutation();
 
-  const handleClickClose = () => {
-    setMenuClass("delete");
-    setTimeout(() => {
-      setIsOpen(false);
-      setMenuClass("open");
-    }, 300);
+  const handleDelete = () => {
+    deleteArtist(id as string).then((e) => {
+      if ("data" in e) {
+        navigate("/");
+      }
+    });
   };
 
   return (
-    <Modal menuClass={menuClass} closeModal={handleClickClose}>
-      <div className={cx("modal", `modal-${theme}`, menuClass)}>
-        <button
-          type="button"
-          onClick={handleClickClose}
-          className={cx("modal__close")}
-        >
-          <Close />
-        </button>
-        <div className={cx("modal__icon")}>
-          <Delete />
-        </div>
-        <p className={cx("modal__title", "base", "md")}>
-          Do you want to delete this artist profile?
-        </p>
-        <p className={cx("modal__text", "small", "lh")}>
-          You will not be able to recover this profile afterwards.
-        </p>
-        <Button
-          onClick={() => deleteArtist(id as string)}
-          theme={theme}
-          className={cx("modal__button")}
-        >
-          Delete
-        </Button>
-        <button
-          type="button"
-          onClick={handleClickClose}
-          className={cx("modal__cancel")}
-        >
-          Cancel
-        </button>
+    <Modal
+      className={cx("modal", `modal-${theme}`)}
+      closeModal={() => setIsOpen(false)}
+      variant="small"
+    >
+      <div className={cx("modal__icon")}>
+        <Delete />
       </div>
+      <p className={cx("modal__title", "base", "md")}>{title}</p>
+      <p className={cx("modal__text", "small", "lh")}>
+        You will not be able to recover this profile afterwards.
+      </p>
+      <Button
+        onClick={handleDelete}
+        theme={theme}
+        className={cx("modal__button")}
+      >
+        Delete
+      </Button>
+      <button
+        type="button"
+        onClick={() => setIsOpen(false)}
+        className={cx("modal__cancel")}
+      >
+        Cancel
+      </button>
     </Modal>
   );
 };
